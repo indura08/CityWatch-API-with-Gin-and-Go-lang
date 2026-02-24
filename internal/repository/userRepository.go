d37b7e5266c9e34ea9af88008cbee9286e4325f6
indura08 <induraperera3@gamil.com>
2026-02-24 13:12:49 +0530

    adding new work related to user and auth part creations

---
+++ internal/repository/userRepository.go
@@ -0,0 +1,13 @@
+package repository
+
+import "gorm.io/gorm"
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+//implement all the repository function elagat patn gattana
