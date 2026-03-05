b029baf075c0c26a10bca2b43c3e65192bb1655e
indura08 <induraperera3@gamil.com>
2026-03-06 01:40:13 +0530

    adding role base access control for each auth endpoint and secure those endpoints

--- internal/middleware/TokenValidation.go
+++ internal/middleware/TokenValidation.go
@@ -1,13 +1,14 @@
 package middleware
 
 import (
+	"log"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func AuthorizeRoles(secret string, allowedRoles ...string) gin.HandlerFunc {
+func AuthorizeRoles(secret string, allowedRoles ...int) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		authHeader := c.GetHeader("Authorization")
@@ -20,10 +21,10 @@ func AuthorizeRoles(secret string, allowedRoles ...string) gin.HandlerFunc {
 		}
 
 		//check the token is valid or not
-		parts := strings.Split(authHeader, "")
+		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 {
 			c.JSON(401, gin.H{
-				"error": "Invalid Token",
+				"error": "Invalid Token1",
 			})
 			c.Abort()
 			return
@@ -35,11 +36,38 @@ func AuthorizeRoles(secret string, allowedRoles ...string) gin.HandlerFunc {
 
 		if err != nil || !token.Valid {
 			c.JSON(401, gin.H{
-				"error": "Invalid Token",
+				"error": "Invalid Token2",
 			})
 			c.Abort()
+			return
 		}
 
 		//need to implement the other parts
+		claims := token.Claims.(jwt.MapClaims)
+
+		rolefloat, ok := claims["role"].(float64)
+		role := int(rolefloat)
+		log.Print("role is :", role)
+
+		if !ok {
+			c.JSON(403, gin.H{
+				"error": "Role couldnt be found in the token",
+			})
+			c.Abort()
+			return
+		}
+
+		//check if the roles are there in the role array api pass krpu array ek me function ekt
+		for _, allowed := range allowedRoles {
+			if role == allowed {
+				c.Next()
+				return
+			}
+		}
+
+		c.JSON(403, gin.H{
+			"Middleware error": "Access denied",
+		})
+		c.Abort()
 	}
 }
